ccc9314ae79d46f5ca6a17db4476479c6b629e9a
ritv1 <117829126+ritv1@users.noreply.github.com>
2024-10-27 16:27:13 +0800

    Update Register.lua

--- Register.lua
+++ Register.lua
@@ -1,4 +1,4 @@
-local Typeface = {}
+local Typeface = {  }
 
 --[[ Compatibility Check ]] do
 	Typeface.Incompatible 	= function() Typeface.Denied = true end
@@ -11,8 +11,8 @@ end
 local Http = cloneref and cloneref(game:GetService 'HttpService') or game:GetService 'HttpService'
 
 -- // Tables
-Typeface.Typefaces = {}
-Typeface.WeightNum = {
+Typeface.Typefaces = {  }
+Typeface.WeightNum = { 
 	["Thin"] = 100,
 
 	["ExtraLight"] = 200, 
@@ -39,43 +39,42 @@ Typeface.WeightNum = {
 function Typeface:Register(Path, Asset)
 	if Typeface.Denied then return warn("Executor is Incompatible For Custom Typeface") end
 
-	local Directory =  `{Path or ""}/{Asset.name}`
+	local Directory = `{ Path or "" }/{ Asset.name }`
+	local Name = `{ Asset.name }{ Asset.weight }{ Asset.style }`
 
-    if not isfile(`{Directory}/{Asset.name}.ttf`) then 
-		writefile(`{Directory}/{Asset.name}.ttf`, Asset.ttf)
+    if not isfile(`{ Directory }/{ Name }.ttf`) then
+		writefile(`{ Directory }/{ Name }.ttf`, base64_decode(game:HttpGet(Asset.ttf)))
 	end
 
-    if not isfile(`{Directory}/Families.json`) then 
-		local Data = {
-			name = `{Asset.Weight} {Asset.Style}`,
-			weight = Typeface.WeightNum[Asset.Weight] or Typeface.WeightNum[string.gsub(Asset.Weight, "%s+", "")],
-			style = string.lower(Asset.Style),
-			assetId = getcustomasset(`{Directory}/{Asset.name}.ttf`)
-		}
+    if not isfile(`{ Directory }/Families.json`) then 
+		local Data = { 
+			name = `{ Asset.weight } { Asset.style }`,
+			weight = Typeface.WeightNum[Asset.weight] or Typeface.WeightNum[string.gsub(Asset.weight, "%s+", "")],
+			style = string.lower(Asset.style),
+			assetId = getcustomasset(`{ Directory }/{ Name }.ttf`)
+		 }
 		
-		local JSONFile = Http:JSONEncode({name = Asset.real_name, faces = {Data}})
+		local JSONFile = Http:JSONEncode({ name = Asset.name, faces = { Data } })
 
-		writefile(`{Directory}/Families.json`, JSONFile)
+		writefile(`{ Directory }/Families.json`, JSONFile)
 	else
-		local JSONFile = Http:JSONDecode(readfile(`{Directory}/Families.json`))
+		local JSONFile = Http:JSONDecode(readfile(`{ Directory }/Families.json`))
 
-		local Data = {
-			name = `{Asset.Weight} {Asset.Style}`,
-			weight = Typeface.WeightNum[Asset.Weight] or Typeface.WeightNum[string.gsub(Asset.Weight, "%s+", "")],
-			style = string.lower(Asset.Style),
-			assetId = getcustomasset(`{Directory}/{Asset.name}.ttf`)
-		}
+		local Data = { 
+			name = `{ Asset.weight } { Asset.style }`,
+			weight = Typeface.WeightNum[Asset.weight] or Typeface.WeightNum[string.gsub(Asset.weight, "%s+", "")],
+			style = string.lower(Asset.style),
+			assetId = getcustomasset(`{ Directory }/{ Name }.ttf`)
+		 }
 
 		table.insert(JSONFile.faces, Data)
-		JSONFile = Http:JSONEncode({name = Asset.real_name, faces = {Data}})
+		JSONFile = Http:JSONEncode({ name = Asset.name, faces = { Data } })
 
-		writefile(`{Directory}/Families.json`, JSONFile)
+		writefile(`{ Directory }/Families.json`, JSONFile)
 	end
 
-	Typeface.Typefaces[Asset.name] = Typeface.Typefaces[Asset.name] or {}
-	Typeface.Typefaces[Asset.name][Asset.weight]
-
-    return Font.new(getcustomasset(`{Directory}/Families.json`))
+	Typeface.Typefaces[Name] = Typeface.Typefaces[Name] or Font.new(getcustomasset(`{ Directory }/Families.json`))
+    return Typeface.Typefaces[Name]
 end
 
 return Typeface
