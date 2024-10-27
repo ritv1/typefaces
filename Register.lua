227a7428a2639a0ae478038d0d479dfa7500feeb
ritv1 <117829126+ritv1@users.noreply.github.com>
2024-10-27 23:32:21 +0800

    Update Register.lua

--- Register.lua
+++ Register.lua
@@ -51,7 +51,7 @@ function Typeface:Register(Path, Asset)
 		writefile(`{ Directory }/{ Name }.font`, game:HttpGet(Asset.link))
 	end
 
-    if not isfile(`{ Directory }/Families.json`) then 
+    if not isfile(`{ Directory }/{ Asset.Name }Families.json`) then 
 		local Data = { 
 			name = `{ Asset.weight } { Asset.style }`,
 			weight = Typeface.WeightNum[Asset.weight] or Typeface.WeightNum[string.gsub(Asset.weight, "%s+", "")],
@@ -61,9 +61,9 @@ function Typeface:Register(Path, Asset)
 		
 		local JSONFile = Http:JSONEncode({ name = Asset.name, faces = { Data } })
 
-		writefile(`{ Directory }/Families.json`, JSONFile)
+		writefile(`{ Directory }/{ Asset.Name }Families.json`, JSONFile)
 	else
-		local JSONFile = Http:JSONDecode(readfile(`{ Directory }/Families.json`))
+		local JSONFile = Http:JSONDecode(readfile(`{ Directory }/{ Asset.Name }Families.json`))
 
 		local Data = { 
 			name = `{ Asset.weight } { Asset.style }`,
@@ -75,10 +75,10 @@ function Typeface:Register(Path, Asset)
 		table.insert(JSONFile.faces, Data)
 		JSONFile = Http:JSONEncode({ name = Asset.name, faces = { Data } })
 
-		writefile(`{ Directory }/Families.json`, JSONFile)
+		writefile(`{ Directory }/{ Asset.Name }Families.json`, JSONFile)
 	end
 
-	Typeface.Typefaces[Name] = Typeface.Typefaces[Name] or Font.new(getcustomasset(`{ Directory }/Families.json`))
+	Typeface.Typefaces[Name] = Typeface.Typefaces[Name] or Font.new(getcustomasset(`{ Directory }/{ Asset.Name }Families.json`))
     return Typeface.Typefaces[Name]
 end
 
